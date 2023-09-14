9adb15d9167d97a7be4834274692a45298179428
Rolando Arellano <rolandoarellano9869@hotmail.com>
2023-09-14 10:30:16 -0700

    se agrego Video modo mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import VideoJade from '../assets/video/PORTADA.mp4'
-import JadeHeroPeek from '../assets/img/JADE 4K  ACCESO.jpg'
-import logoPeek from '../assets/img/logo Jade blanco-02.png'
+import JadeHeroVideo from '../assets/video/ANIMACION JADE.mp4'
+
 const Hero = () => {
   return (
-    <figure className='relative bg-black flex items-center justify-center'>
+    // Video para Escritorio
+    <figure className='relative bg-white flex items-center justify-center'>
       <video
-        kind="captions" 
-    
+        kind="captions"
         label="spanish_captions"
         autoPlay
         loop
         muted
-        src={VideoJade}></video>
+        src={VideoJade} className='hidden md:block '></video>
+      {/* Video para mobile */}
+      <video
+        kind="captions"
+        label="spanish_captions"
+        autoPlay
+        loop
+        muted
+        src={JadeHeroVideo} className='block  md:hidden  bg-white border border-gray-200 rounded-lg'></video>
+
+
+
       {/* <div className='absolute z-10'>
         <img src={logoPeek} alt="logo" className='w-[5rem] md:w-[25rem]' />
       </div>
